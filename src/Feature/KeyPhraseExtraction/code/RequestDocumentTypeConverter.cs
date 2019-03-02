04c729f1f5281a33b164773560f0b989375ae82a
Kelly Brennan <Kelly_Brennan@epam.com>
2019-03-02 12:53:40 -0500

    * Default implementation of the ReqyestDocumentTypeConverter. Replicating the POC code here which causes a duplication in fetching items from the index.

---
+++ src/Feature/KeyPhraseExtraction/code/RequestDocumentTypeConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DoctaCore.Feature.KeyPhraseExtraction.Models;
+using DoctaCore.Feature.KeyPhraseExtraction.Shell.Framework.Commands;
+using DoctaCore.Foundation.KeyPhrases;
+using DoctaCore.Foundation.KeyPhrases.Models;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.ContentSearch.Security;
+using Sitecore.Data.Items;
+
+namespace DoctaCore.Feature.KeyPhraseExtraction
+{
+    public class RequestDocumentTypeConverter : ITypeConverter<IEnumerable<Item>, RequestDocumentCollection>
+    {
+        public RequestDocumentCollection Convert(IEnumerable<Item> obj)
+        {
+            var results = new List<RequestDocument>();
+            foreach (var item in obj)
+            {
+                using (var searchContext = ContentSearchManager.GetIndex((IIndexable)item)
+                    .CreateSearchContext(SearchSecurityOptions.DisableSecurityCheck))
+                {
+                    var searchResult = searchContext.GetQueryable<ExtendedSearchResultItem>()
+                        .Where(x => x.ItemId == item.ID && x.Language == item.Language.Name && x.LatestVersion).GetResults().FirstOrDefault();
+                    if (searchResult != null && searchResult.Document != null && !string.IsNullOrWhiteSpace(searchResult.Document.Content))
+                    {
+                        var document = new RequestDocument();
+                        document.Text = searchResult.Document.Content;
+                        document.Id = item.ID.Guid;
+                        document.Language = item.Language.Name;
+
+                        results.Add(document);
+                    }
+                }
+            }
+
+            return (RequestDocumentCollection)results;
+        }
+    }
+}
