3aded517e08b086c697b0e734bbd547efd637be0
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2019-03-02 11:42:28 -0500

    Added abstract class for response handler

---
+++ src/Foundation/KeyPhrases/code/BaseResponseHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Pipelines;
+
+namespace DoctaCore.Foundation.KeyPhrases
+{
+    public abstract class BaseResponseHandler<TResponseModelCollection, TResponseModel, TPipelineArgs> : IHandleResponse<TResponseModelCollection, TResponseModel>
+        where TResponseModelCollection : IEnumerable<TResponseModel>
+        where TPipelineArgs : PipelineArgs
+    {
+        public virtual void DoHandleResponse(TResponseModelCollection collection)
+        {
+            var args = GetArgs(collection);
+            CorePipeline.Run("handleKeyPhrasesResponse", args);
+        }
+
+        public abstract TPipelineArgs GetArgs(TResponseModelCollection collection);
+    }
+}
