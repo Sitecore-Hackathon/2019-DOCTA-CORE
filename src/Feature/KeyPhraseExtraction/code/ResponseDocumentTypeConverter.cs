049d06f5d8f452c68103da6d8574a4b8d029b808
Kelly Brennan <Kelly_Brennan@epam.com>
2019-03-02 13:19:49 -0500

    * Scaffold for Response Document convertor

---
+++ src/Feature/KeyPhraseExtraction/code/ResponseDocumentTypeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DoctaCore.Feature.KeyPhraseExtraction.Models;
+using DoctaCore.Foundation.KeyPhrases;
+
+namespace DoctaCore.Feature.KeyPhraseExtraction
+{
+    public class ResponseDocumentTypeConverter : ITypeConverter<string, ResponseDocument>
+    {
+        public ResponseDocument Convert(string obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
