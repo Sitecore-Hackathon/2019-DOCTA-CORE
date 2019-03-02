beadce6593097dc2d0a80bfe8db6d793583a026a
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2019-03-02 02:14:51 -0500

    Added base classes to support rules engine integration/extension

---
+++ src/Foundation/Rules/code/Conditions/BaseOperatorCondition{TRuleContext}.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Rules;
+
+namespace DoctaCore.Foundation.Rules.Conditions
+{
+    /// <summary>
+    /// The base class for the rules engine condition
+    /// </summary>
+    /// <typeparam name="TRuleContext">
+    /// Type providing rule context.
+    /// </typeparam>
+    [UsedImplicitly]
+    public abstract class BaseOperatorCondition<TRuleContext> : Sitecore.Rules.Conditions.OperatorCondition<TRuleContext>
+        where TRuleContext : RuleContext
+    { 
+        /// <summary>
+        /// Condition implementation.
+        /// </summary>
+        /// <param name="ruleContext">The rule context.</param>
+        /// <returns>
+        /// True if the item has layout details for the default device,
+        /// otherwise False.
+        /// </returns>
+        protected sealed override bool Execute([NotNull] TRuleContext ruleContext)
+        {
+            Assert.IsNotNull(ruleContext, "ruleContext");
+            if (ruleContext.IsAborted)
+            {
+                return false;
+            }
+
+            try
+            {
+                Log.Debug("RuleCondition " + GetType().Name + " started", this);
+
+                var result = ExecuteCondition(ruleContext);
+                return result;
+            }
+            catch (Exception exception)
+            {
+                Log.Error("RuleCondition " + GetType().Name + " failed.", exception, this);
+            }
+
+            Log.Debug("RuleCondition " + GetType().Name + " ended", this);
+
+            return false;
+        }
+
+        /// <summary>
+        /// The execute rule.
+        /// </summary>
+        /// <param name="ruleContext">The rule context.</param>
+        /// <returns>
+        /// <c>True</c>, if the condition succeeds, otherwise <c>false</c>.
+        /// </returns>
+        protected abstract bool ExecuteCondition([NotNull] TRuleContext ruleContext);
+    }
+}
---
+++ src/Foundation/Rules/code/RuleContext{TArgs}.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Rules;
+
+namespace DoctaCore.Foundation.Rules
+{
+	public class RuleContext<TArgs> : RuleContext, IRuleContext<TArgs>
+		where TArgs : class
+	{
+		public RuleContext(TArgs args)
+		{
+			this.Args = args;
+		}
+
+		/// <summary>
+		/// Gets the args.
+		/// </summary>
+		public TArgs Args { get; }
+	}
+}
