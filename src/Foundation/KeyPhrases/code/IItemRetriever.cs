15808bb9d3c3e55c8871c9e55d81f6d48c256f4e
Zachary_Kniebel <Zachary_Kniebel@epam.com>
2019-03-02 11:22:16 -0500

    Added in KeyPhrases foundation layer abstraction to be wired up shortly

---
+++ src/Foundation/KeyPhrases/code/IItemRetriever.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data.Items;
+
+namespace DoctaCore.Foundation.KeyPhrases
+{
+    public interface IItemRetriever
+    {
+        IEnumerable<Item> GetItems();
+    }
+}
---
+++ src/Foundation/KeyPhrases/code/ITypeConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoctaCore.Foundation.KeyPhrases
+{
+    public interface ITypeConverter<in TIn, out TOut>
+    {
+        TOut Convert(TIn obj);
+    }
+}
